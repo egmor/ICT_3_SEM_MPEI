b0332ab2052cc0621159dc6effa51b04737ff39e
Egor <egmor07@gmail.com>
2026-09-14 18:58:19 +0300

    Сделан ручной ввод

--- ICT_3_SEM_LAB_1/ICT_3_SEM_LAB_1/ICT_3_SEM_LAB_1.cpp
+++ ICT_3_SEM_LAB_1/ICT_3_SEM_LAB_1/ICT_3_SEM_LAB_1.cpp
@@ -13,21 +13,37 @@ bool correct_day(int day, int month, int year) {
     else if (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) return false;
     else if (day > 29 && month == 2 && (year % 4 == 0)) return false;
     else if (day > 28 && month == 2 && (year % 4 != 0)) return false;
-    else { return true; }
+    else return true;
 }
 
 bool correct_date(Date_Last_Visit& date) {
     if (date.year <= 2000 || date.year > 2026) return false;
     if (date.month > 12 || date.month < 1) return false;
     if (!correct_day(date.day, date.month, date.year)) return false;
-    else { return true; }
+    else return true;
+}
+
+bool correct_address(Address& data) {
+    if (data.city.size() < 2 || data.city.size() > 10) return false;
+    if (data.district.size() < 2 || data.district.size() > 10) return false;
+    if (data.street.size() < 2 || data.street.size() > 15) return false;
+    if (std::stoi(data.building) < 1 || std::stoi(data.building) > 150) return false;
+    if (std::stoi(data.apartment) < 1 || std::stoi(data.apartment) > 1000) return false;
+    else return true;
+}
+
+bool correct_name(Patient& data) {
+    if (data.name.size() < 2 || data.name.size() > 10) return false;
+    if (data.surname.size() < 2 || data.surname.size() > 10) return false;
+    if (data.last_name.size() < 5 || data.last_name.size() > 15) return false;
+    else return true;
 }
 
 std::string to_lower(std::string str) {
     for (char& ch : str) {
         unsigned char uc = static_cast<unsigned char>(ch);
-        if (uc >= 192 && uc <= 223) ch = static_cast<char>(uc + 32);
-        else if (uc == 168)  ch = static_cast<char>(184);
+        if (uc >= 192 && uc <= 223) ch = static_cast<char>(uc + 32); //Ё
+        else if (uc == 168)  ch = static_cast<char>(184); //ё
     }
     return str;
 }
@@ -44,8 +60,7 @@ bool text_to_queue(const std::string& file_name, Queue& q) {
         >> temp.address.building >> temp.address.apartment
         >> temp.date.day >> temp.date.month >> temp.date.year
         >> temp.birth_year >> temp.illness) {
-
-        if (correct_date(temp.date)) push_queue(q, temp);
+        if (correct_date(temp.date) && correct_address(temp.address) && correct_name(temp.SNL)) push_queue(q, temp);
         else ++skippedCount;
     }
 
@@ -89,6 +104,63 @@ bool over_3_months(const Date_Last_Visit& date) {
     return false;
 }
 
+void hand_enter(DoublyLinkedList& list) {
+    Medical_Record temp{};
+    //ФИО
+    std::cout << "\nВведите ФИО пациента (Имя Фамилия Отчество: ";
+    std::cin >> temp.SNL.name;
+    std::cin >> temp.SNL.surname;
+    std::cin >> temp.SNL.last_name;
+    while (!correct_name(temp.SNL)) {
+        std::cout << "\nНекорректный ввод ФИО пациента\n Введите ещё раз: ";
+        std::cin >> temp.SNL.name;
+        std::cin >> temp.SNL.surname;
+        std::cin >> temp.SNL.last_name;
+    }
+    //Адрес
+    std::cout << "\nВведите адрес проживания пациента (город, район, улица, дом, квартира): ";
+    std::cin >> temp.address.city;
+    std::cin >> temp.address.district;
+    std::cin >> temp.address.street;
+    std::cin >> temp.address.building;
+    std::cin >> temp.address.apartment;
+    while (!correct_address(temp.address)) {
+        std::cout << "\nНекорректный ввод адрес прожвания пациента\n Введите ещё раз: ";
+        std::cin >> temp.address.city;
+        std::cin >> temp.address.district;
+        std::cin >> temp.address.street;
+        std::cin >> temp.address.building;
+        std::cin >> temp.address.apartment;
+    }
+    //Дата последнего посещения
+    std::cout << "\nВведите дату последнего посещения пациента (дд мм гггг): ";
+    std::cin >> temp.date.day;
+    std::cin >> temp.date.month;
+    std::cin >> temp.date.year;
+    while (!correct_date(temp.date)) {
+        std::cout << "\nНекорректный ввод дату последнего посещения пациента\n Введите ещё раз: ";
+        std::cin >> temp.date.day;
+        std::cin >> temp.date.month;
+        std::cin >> temp.date.year;
+    }
+    //Год рождения
+    std::cout << "\nВведите год рождения пациента: ";
+    std::cin >> temp.birth_year;
+    while (temp.birth_year < 1926 || temp.birth_year > 2026) {
+        std::cout << "\nНекорректный ввод года рождения пациента\n Введите ещё раз: ";
+        std::cin >> temp.birth_year;
+    }
+    //Болезнь
+    std::cout << "\nВведите болезнь пациента: ";
+    std::cin >> temp.illness;
+
+    push_back(list, temp);
+
+    Node* last_record = list.tail;
+
+    table_sick_man(last_record->data);
+}
+
 int main() {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
@@ -114,26 +186,32 @@ int main() {
     }
 
     int choice{ 0 };
-    std::cout << "\nВыберите вариант сортировки:\n"
-        << " 1. По ФИО\n"
-        << " 2. По году рождения\n"
-        << " 3. По адресу\n"
-        << " 4. По заболеванию\n"
-        << " 5. По дате посещения\n"
-        << " Введите номер: ";
-    std::cin >> choice;
+    do {
+        std::cout << "\nВыберите вариант работы программы:\n"
+            << " 1. По ФИО\n"
+            << " 2. По году рождения\n"
+            << " 3. По адресу\n"
+            << " 4. По заболеванию\n"
+            << " 5. По дате посещения\n"
+            << " 6. Ручной ввод данных\n"
+            << " 0. Завершить работу\n"
+            << " Введите номер: ";
+        std::cin >> choice;
 
-    switch (choice) {
-    case 1: sort_by_fio(list); break;
-    case 2: sort_by_birth_year(list); break;
-    case 3: sort_by_address(list); break;
-    case 4: sort_by_illness(list); break;
-    case 5: sort_by_date(list); break;
-    default:
-        std::cerr << "[Ошибка] Неверный выбор!\n";
-        clear_list(list);
-        return 1;
-    }
+        switch (choice) {
+        case 1: sort_by_fio(list); break;
+        case 2: sort_by_birth_year(list); break;
+        case 3: sort_by_address(list); break;
+        case 4: sort_by_illness(list); break;
+        case 5: sort_by_date(list); break;
+        case 6: hand_enter(list); break;
+        case 0: std::cout << "\nРабота программы завершена\n"; return 0;
+        default:
+            std::cerr << "[Ошибка] Неверный выбор!\n";
+            clear_list(list);
+            return 1;
+        }
+    } while (choice == 6);
 
     Queue output_queue;
     Queue diabet_queue;
